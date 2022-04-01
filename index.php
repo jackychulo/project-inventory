8b883f7248e7470f487f90e1ec158ebc6247b504
jackychulo <70356348+jackychulo@users.noreply.github.com>
2022-03-31 21:25:39 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <header>
-    <title> Anny 's Inventory</title>
+    <title>Chulo's Inventory</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="style/style.css" rel="stylesheet" type="text/css">
